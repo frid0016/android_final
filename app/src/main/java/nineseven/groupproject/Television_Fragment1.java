e9bcd414375fa57caea4144ad1d32c7c6585d67c
Nastya <frid0016@algonquinlive.com>
2016-11-28 21:48:26 -0500

    Fragments for activity_television

---
+++ app/src/main/java/nineseven/groupproject/Television_Fragment1.java
@@ -0,0 +1,17 @@
+package nineseven.groupproject;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Television_Fragment1 extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        //return super.onCreateView(inflater, container, savedInstanceState);
+        View rootView = inflater.inflate(R.layout.television_fragment1, container, false);
+        return rootView;
+    }
+}
