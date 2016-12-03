41222c7314a91d93a09a9d943899a2d987bf264c
Michael Palmer <palm0132@algonquinlive.com>
2016-12-03 14:10:54 -0500

    Toolbar updates

--- app/src/main/java/nineseven/groupproject/HouseActivity.java
+++ app/src/main/java/nineseven/groupproject/HouseActivity.java
@@ -1,21 +1,30 @@
 package nineseven.groupproject;
 
+import android.app.Activity;
+import android.app.Dialog;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Configuration;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class HouseActivity extends AppCompatActivity {
     ListView listView;
@@ -32,6 +41,8 @@ public class HouseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_house);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.House_Toolbar);
+        setSupportActionBar(toolbar);
 
         container = (FrameLayout) findViewById(R.id.container);
         listView = (ListView) findViewById(R.id.House_List_View);
@@ -131,5 +142,43 @@ public class HouseActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu_launch_screen, menu);
         return true;
     } // end of method onCreateOptionsMenu
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        AlertDialog.Builder builder;
+        switch(item.getItemId()) {
+            case R.id.House_Menu_Item:
+                Intent houseIntent = new Intent(HouseActivity.this, HouseActivity.class);
+                startActivity(houseIntent);
+                break;
+            case R.id.Living_Room_Menu_Item:
+                Intent livingRoomIntent = new Intent(HouseActivity.this, LivingRoomActivity.class);
+                startActivity(livingRoomIntent);
+                break;
+            case R.id.Kitchen_Menu_Item:
+                Intent kitchenIntent = new Intent(HouseActivity.this, KitchenActivity.class);
+                startActivity(kitchenIntent);
+                break;
+            case R.id.Automobile_Menu_Item:
+                Intent automobileIntent = new Intent(HouseActivity.this, AutomobileActivity.class);
+                startActivity(automobileIntent);
+                break;
+            case R.id.Help_Menu_Item:
+                builder = new AlertDialog.Builder(this);
+                LayoutInflater inflater = this.getLayoutInflater();
+                builder.setTitle(R.string.dialog_help_title);
+                builder.setView(inflater.inflate(R.layout.dialog_house, null))
+                        .setNegativeButton(R.string.dialog_help_button, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                // User clicked OK
+                            }
+                        });
+                AlertDialog dialog = builder.create();
+                dialog.show();
+                break;
+        }
+        return true;
+    }
 
 }
--- app/src/main/java/nineseven/groupproject/HouseTemperatureActivity.java
+++ app/src/main/java/nineseven/groupproject/HouseTemperatureActivity.java
@@ -1,8 +1,12 @@
 package nineseven.groupproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class HouseTemperatureActivity extends AppCompatActivity {
 
@@ -10,6 +14,15 @@ public class HouseTemperatureActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_house_temperature);
+
+        Button htSetButton = (Button) findViewById(R.id.htSetButton);
+
+        htSetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "House temperature has been set", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
     @Override
