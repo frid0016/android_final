c1131e642bd5106e25b66ce1300282dffdeffcb7
Nastya <frid0016@algonquinlive.com>
2016-12-08 16:42:17 -0500

    launch screen change

--- app/src/main/java/nineseven/groupproject/LaunchScreenActivity.java
+++ app/src/main/java/nineseven/groupproject/LaunchScreenActivity.java
@@ -1,9 +1,12 @@
 package nineseven.groupproject;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -26,12 +29,6 @@ public class LaunchScreenActivity extends AppCompatActivity {
         return true;
     } // end of method onCreateOptionsMenu
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem menuItem) {
-        int id = menuItem.getItemId();
-        return super.onOptionsItemSelected(menuItem);
-
-    } // end of method onOptionsItemSelected
 
     public void launchHouseOnClick (View view) {
         Intent intent = new Intent(LaunchScreenActivity.this, HouseActivity.class);
@@ -53,4 +50,42 @@ public class LaunchScreenActivity extends AppCompatActivity {
         startActivity(intent);
     } // end of method launchAutomobileOnClick
 
-} // end of class LaunchScreenActivity
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        AlertDialog.Builder builder;
+        switch(item.getItemId()) {
+            case R.id.House_Menu_Item:
+                Intent houseIntent = new Intent(LaunchScreenActivity.this, HouseActivity.class);
+                startActivity(houseIntent);
+                break;
+            case R.id.Living_Room_Menu_Item:
+                Intent livingRoomIntent = new Intent(LaunchScreenActivity.this, LivingRoomActivity.class);
+                startActivity(livingRoomIntent);
+                break;
+            case R.id.Kitchen_Menu_Item:
+                Intent kitchenIntent = new Intent(LaunchScreenActivity.this, Kitchen_Activity.class);
+                startActivity(kitchenIntent);
+                break;
+            case R.id.Automobile_Menu_Item:
+                Intent automobileIntent = new Intent(LaunchScreenActivity.this, AutomobileActivity.class);
+                startActivity(automobileIntent);
+                break;
+            case R.id.Help_Menu_Item:
+                builder = new AlertDialog.Builder(this);
+                LayoutInflater inflater = this.getLayoutInflater();
+                builder.setTitle(R.string.dialog_help_title);
+                builder.setView(inflater.inflate(R.layout.dialog_launch, null))
+                        .setNegativeButton(R.string.dialog_help_button, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                // User clicked OK
+                            }
+                        });
+                AlertDialog dialog = builder.create();
+                dialog.show();
+                break;
+        }
+        return true;
+    } // end of method onOptionsItemSelected
+
+}
+
